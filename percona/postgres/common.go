ff936c7b852cfb53caa94a7d1581884fcc202059
Akshit Garg <akshit@fly.io>
2025-05-30 08:48:30 +0530

    Thanks supermaven
    Signed-off-by: Akshit Garg <akshit@fly.io>

--- percona/postgres/common.go
+++ percona/postgres/common.go
@@ -3,6 +3,7 @@ package perconaPG
 import (
 	"context"
 
+	gover "github.com/hashicorp/go-version"
 	"github.com/pkg/errors"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/labels"
@@ -15,23 +16,29 @@ func GetPrimaryPod(ctx context.Context, cli client.Client, cr *v2.PerconaPGClust
 	podList := &corev1.PodList{}
 	// K8SPG-648: patroni v4.0.0 deprecated "master" role.
 	//            We should use "primary" instead
-	err := cli.List(ctx, podList, &client.ListOptions{
+	role := "primary"
+	patroniVer, err := gover.NewVersion(cr.Status.PatroniVersion)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get patroni version")
+	}
+	patroniVer4 := patroniVer.Compare(gover.Must(gover.NewVersion("4.0.0"))) >= 0
+	if !patroniVer4 {
+		role = "master"
+	}
+	err = cli.List(ctx, podList, &client.ListOptions{
 		Namespace: cr.Namespace,
 		LabelSelector: labels.SelectorFromSet(map[string]string{
-			"app.kubernetes.io/instance":                             cr.Name,
-			"postgres-operator.crunchydata.com/pgbackrest-dedicated": "",
+			"app.kubernetes.io/instance":             cr.Name,
+			"postgres-operator.crunchydata.com/role": role,
 		}),
 	})
-	if err != nil {
-		return nil, err
-	}
 
 	if len(podList.Items) == 0 {
-		return nil, errors.New("no repo-host pod found")
+		return nil, errors.New("no primary pod found")
 	}
 
 	if len(podList.Items) > 1 {
-		return nil, errors.New("multiple repo-host pods found")
+		return nil, errors.New("multiple primary pods found")
 	}
 
 	return &podList.Items[0], nil
@@ -42,8 +49,8 @@ func GetRepoHostPod(ctx context.Context, cli client.Client, cr *v2.PerconaPGClus
 	err := cli.List(ctx, podList, &client.ListOptions{
 		Namespace: cr.Namespace,
 		LabelSelector: labels.SelectorFromSet(map[string]string{
-			"app.kubernetes.io/instance":             cr.Name,
-			"postgres-operator.crunchydata.com/role": "repo-host",
+			"app.kubernetes.io/instance":                             cr.Name,
+			"postgres-operator.crunchydata.com/pgbackrest-dedicated": "",
 		}),
 	})
 	if err != nil {
