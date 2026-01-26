9ef6137d106b120700acf1486061cfeeb9f23adc
Juliana Oliveira <juliana@fly.io>
2026-01-26 20:35:11 -0300

    wip

--- internal/pgbouncer/config.go
+++ internal/pgbouncer/config.go
@@ -120,6 +120,8 @@ func clusterINI(cluster *v1beta1.PostgresCluster) string {
 		"server_tls_sslmode": "verify-full",
 		"server_tls_ca_file": certBackendAuthorityAbsolutePath,
 
+		// Disable Unix sockets to keep the filesystem read-only.
+		"unix_socket_dir": "",
 	}
 
 	// Override the above with any specified settings.
